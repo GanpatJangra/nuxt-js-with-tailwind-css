bbc8d5161d7326f76e4c7ef40604c95824de256a
Ganpat Jangra <69577968+GanpatJangra@users.noreply.github.com>
2022-05-15 03:16:44 +0530

    Add files via upload
    ✌️

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="px-3 sm:px-16 lg:px-[100px] text-third bg-[#FEFCEC] scroll-smooth">
+    <nav-bar></nav-bar>
+    <nuxt/>
+    <footer-bar></footer-bar>
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'blank nuxt js',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Education website created by Ganpat Jangra using tailwindcss and nuxt js ' },
+      { name: 'author', name: 'author', content: 'Ganpat jangra ' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~/assets/css/tailwind.css"],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    'vue-ssr-carousel/nuxt',
+    // 'nuxt-vite',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // '@splidejs/splide',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
